67c76ed2bb24ed3e894b67885fe8a292a77c3768
Ophelia <wenmengofficial@gmail.com>
2025-04-09 23:58:58 +0800

    Update 1. Variavles.py

--- Basis/1. Variavles.py
+++ Basis/1. Variavles.py
@@ -41,17 +41,3 @@ c2 = a - b
 c3 = a * b
 c4 = a / b
 print(c1,c2,c3,c4) 
-# 练习：类的模块的继承与不同模块的传递
-a,b,c = '西红柿','黄瓜','大葱'
-a,b,c = 10,2.99,4.7
-d = a*3 + b*4 + c*2
-print(d)
-xhs_price,amount1 = 10,3
-hg_price,amount2 = 2.99,4
-dc_price,amount3 = 4.7,2
-total_price = xhs_price * amount1 + hg_price * amount2 + dc_price * amount3
-print(total_price)
-# 4、变量的类型：字符串str，整数int，小数浮点数float，检测数据类型用函数type，即print(type(message))，对应的转换函数为int(),float(),str()
-a = '45'
-b = int(a)
-print(type(b))
