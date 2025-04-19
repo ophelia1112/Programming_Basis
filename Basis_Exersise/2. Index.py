bca774487d12d09d770f074a445b62b5f9bec665
Ophelia <wenmengofficial@gmail.com>
2025-04-19 18:50:24 +0800

    Delete Basis_Exersise directory

--- Basis_Exersise/2. Index.py
+++
@@ -1,7 +0,0 @@
-letters = ['a','b','c','d','e','f','g','h','i','j']
-print(letters[1]) # 序列索引
-print(letters[3:6]) # 序列切片，python不包含第二个位置，要多写一个
-print(letters[:3]) # 序列前位置切片，别忘了第一个位置是0
-print(letters[-2]) # 负数索引直接是-1计数
-print(letters[-3:]) # 负数切片
-print(letters[::2]) # 步长取数法，默认是一步长，一步取一个
